7d0a3cfa5c2f3cee10d43f37f595c7bb9b37518d
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-18 02:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function wakeDog(dogName, dogBreed) {
 
 function leashDog(dogName, dogBreed) { 
   console.log("Leash ${dogName} the ${dogBreed}");
-  return ``
+  return `Leash ${dogName} the ${dogBreed}`
 } 
 
 function walkToPark(dogName, dogBreed) { 
