2e3ff7610e2f533e635ba3f9658ff1e3c1226407
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-18 02:16:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ function unleashDog(dogName, dogBreed) {
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
-  return routine.map(dogName, dogBreed)
+  return routine.map(fn =>dogName, dogBreed)
 }
