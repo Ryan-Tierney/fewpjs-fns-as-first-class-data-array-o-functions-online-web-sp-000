be7757c25faaac665100fa0853abc567ed11be17
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-18 02:09:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ function exerciseDog(dogName, dogBreed) {
 }
 
 function wakeDog(dogName, dogBreed) { 
-  console.log("Wake ${dogName} the #{dogBreed}");
+  console.log("Wake ${dogName} the ${dogBreed}");
 } 
 
 function leashDog(dogName, dogBreed) { 
-  console.log("");
+  console.log("Leash ${dogName} the ${dogBreed}");
 } 
 
 function walkToPark(dogName, dogBreed) { 
-  console.log("");
+  console.log("Walk to the park with ${dogName} the ${dogBreed}");
 }
 
 function throwFrisbee(dogName, dogBreed) { 
-  console.log("");
+  console.log("throw the frisbee for ${dogName} the ${dogBreed}");
 }
 
 function walkHome(dogName, dogBreed) { 
-  console.log("");
+  console.log("Walk home with ${dogName} the #{dogBreed}");
 }
 
 function unleashDog(dogName, dogBreed) { 
-  console.log("");
+  console.log("Unleash ${dogName} the ${dogBreed}");
 }
